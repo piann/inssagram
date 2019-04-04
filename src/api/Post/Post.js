b8c5fe7bfd6b877f37ae601d59af4d20c8ff5b11
root <root@localhost.localdomain>
2019-04-04 08:12:09 -0400

    refactoring getPostInfo

--- src/api/Post/Post.js
+++ src/api/Post/Post.js
@@ -3,16 +3,45 @@ import { prisma } from "../../../generated/prisma-client";
 // This file is for computed field
 
 export default {
-    Post:{
-        isLiked: async (parent, args, {request}) => {
-            const {user} = request;
-            const {id} = parent;
-            return prisma.$exists.like({
-                AND:[
-                    {user:{id:user.id}}, {post:{id}}
-                ]
-            })
-        }
-    }
+    Post: {
+      likes: ({ id }) => prisma.post({ id }).likes(),
+      files: ({ id }) => prisma.post({ id }).files(),
+      comments:  ({ id }) => prisma.post({ id }).comments(),
+      user: ({ id }) => prisma.post({ id }).user(),
 
-}
+      isLiked: (parent, _, {request}) => {
+        const { user } = request;
+        const { id } = parent;
+        return prisma.$exists.like({
+          AND: [
+            {
+              user: {
+                id: user.id
+              }
+            },
+            {
+              post: {
+                id
+              }
+            }
+          ]
+        });
+      },
+
+      likeCount: parent =>
+        prisma
+          .likesConnection({
+            where: { post: { id: parent.id } }
+          })
+          .aggregate()
+          .count(),
+
+      commentCount: parent =>
+        prisma
+          .commentsConnection({
+            where: { post: { id: parent.id } }
+          })
+          .aggregate()
+          .count()
+    }
+  };
--- src/api/Post/getPostInfo/getPostInfo.js
+++ src/api/Post/getPostInfo/getPostInfo.js
@@ -6,19 +6,9 @@ export default {
         getPostInfo: async(_, args,)=>{
 
             try{
-
                 const {id} = args;
-                const post = await prisma.post({id});
-                const comments = await prisma.post({id}).comments().$fragment(COMMENT_FRAGMENT);
-                const likeCount = await prisma.likesConnection(
-                    {where:{post:{id}}}
-                    ).aggregate().count();
-                const files = await prisma.post({id}).files();
-                const user = await prisma.post({id}).user();
-                return {
-                    post, comments, likeCount,files,user
-                }
-
+                return await prisma.post({id});
+                
             }catch(err){
                 console.log(err);
                 return null
