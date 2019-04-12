be4b37d3d37b986ae8143aef583bcbaed4782987
root <root@localhost.localdomain>
2019-04-12 08:32:34 -0400

    add some computed field for user

--- src/api/User/User.js
+++ src/api/User/User.js
@@ -3,6 +3,27 @@ import { prisma } from "../../../generated/prisma-client";
 
 export default{
     User:{
+        posts: ({ id }) => prisma.user({ id }).posts(),
+        followings: ({ id }) => prisma.user({ id }).followings(),
+        followers: ({ id }) => prisma.user({ id }).followers(),
+        likes: ({ id }) => prisma.user({ id }).likes(),
+        comments: ({ id }) => prisma.user({ id }).comments(),
+        rooms: ({ id }) => prisma.user({ id }).rooms(),
+        postsCount: ({ id }) =>
+          prisma
+            .postsConnection({ where: { user: { id } } })
+            .aggregate()
+            .count(),
+        followingsCount: async ({ id }) =>
+        prisma
+            .usersConnection({ where: { followers_some: { id } } })
+            .aggregate()
+            .count(),
+        followersCount: ({ id }) =>
+          prisma
+            .usersConnection({ where: { followings_some: { id } } })
+            .aggregate()
+            .count(),
         fullName : parent => `${parent.firstName} ${parent.lastName}`,
         isFollowing : async (parent, args, {request})=> {
             const {user} = request;
