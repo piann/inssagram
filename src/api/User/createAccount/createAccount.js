4ca85cb0eb43d9857f139ac524730616be4b115a
root <root@localhost.localdomain>
2019-04-19 08:04:44 -0400

    fix API for createAccount : check existing userName

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -4,6 +4,11 @@ export default{
     Mutation:{
         createAccount: async(_, args) => {
             try{
+                const exists = await prisma.$exists.user({userName});
+                if(exists){
+                    throw Error("This userName is already used")
+                    return
+                }
                 const {userName, email, firstName = "", lastName = "", bio = ""} = args;
                 const user = await prisma.createUser({
                     userName,
